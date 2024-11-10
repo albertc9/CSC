9db451896eee08f1d12dc69dd07ee89835277167
“albertc9” <zlk20050305@gmail.com>
2024-11-11 01:18:54 +0800

    update reglncr.py and stimulate.py (from test11.py and test12.py)

---
+++ pymtl3_test/test1/RegIncr.py
@@ -0,0 +1,25 @@
+from pymtl3 import *
+
+class RegIncr(Component):
+
+    def construct(s):
+
+        # 端口接口
+        s.in_ = InPort(Bits8)
+        s.out = OutPort(Bits8)
+
+        # 内部信号
+        s.reg_out = Wire(8)
+
+        # 寄存器逻辑
+        @update_ff
+        def block1():
+            if s.reset:
+                s.reg_out <<= 0
+            else:
+                s.reg_out <<= s.in_
+
+        # 递增逻辑
+        @update
+        def block2():
+            s.out @= s.reg_out + 1
---
+++ pymtl3_test/test1/Stimulate.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+#=========================================================================
+# regincr-sim <input-values>
+#=========================================================================
+from pymtl3 import *
+from sys import argv
+from RegIncr import RegIncr
+
+# 从命令行获取输入值列表
+input_values = [int(x, 0) for x in argv[1:]]
+
+# 在输入值列表末尾添加三个零值，以便模拟多几个周期，方便观察输出
+input_values.extend([0] * 3)
+
+model = RegIncr()
+model.elaborate()
+
+# 应用默认的通用 Pass 组以添加模拟功能
+model.apply(DefaultPassGroup())
+
+# 重置模拟器
+model.sim_reset()
+
+# 输入值应用与输出值显示
+for input_value in input_values:
+    model.in_ @= input_value
+    model.sim_eval_combinational()
+    
+    # 显示当前周期的输入与输出
+    print(f"cycle = {model.sim_cycle_count()}: in = {model.in_}, out = {model.out}")
+    
+    # 模拟器前进一个周期
+    model.sim_tick()
